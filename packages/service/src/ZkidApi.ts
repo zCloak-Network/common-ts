093c5304190986c26d68fc900a9649ce2e1ebacd
Zhichao Zhang <zzcwoshizz@163.com>
2022-06-28 15:38:28 +0800

    zkid api add chainId param

--- packages/service/src/ZkidApi.ts
+++ packages/service/src/ZkidApi.ts
@@ -2,33 +2,53 @@ import type { Activity, Config, MintPoap, Proof, ProofProcess, ServerResponse } 
 
 import { combineConfig, Request } from './request';
 
+function combineChainId(params: Record<string, any>, chainId: number) {
+  return {
+    ...params,
+    chainId
+  };
+}
+
 export class ZkidApi extends Request {
-  constructor(endpoint: string, config: Config = {}) {
+  public chainId: number;
+
+  constructor(endpoint: string, config: Config = {}, chainId: number) {
     super(
       endpoint,
       combineConfig(config, {
         mode: 'cors'
       })
     );
+    this.chainId = chainId;
   }
 
   getMintPoap(params: { who: string }) {
-    return this.get<ServerResponse<MintPoap | null>>('/mint-poap', { params });
+    return this.get<ServerResponse<MintPoap | null>>('/mint-poap', {
+      params: combineChainId(params, this.chainId)
+    });
   }
 
   proofProcess(params: { dataOwner: string; requestHash: string }) {
-    return this.get<ServerResponse<ProofProcess>>('/proof/process', { params });
+    return this.get<ServerResponse<ProofProcess>>('/proof/process', {
+      params: combineChainId(params, this.chainId)
+    });
   }
 
   userProof(params: { dataOwner: string }) {
-    return this.get<ServerResponse<Proof[]>>('/user/proof', { params });
+    return this.get<ServerResponse<Proof[]>>('/user/proof', {
+      params: combineChainId(params, this.chainId)
+    });
   }
 
   userActivities(params: { dataOwner: string }) {
-    return this.get<ServerResponse<Activity[]>>('/user/activies', { params });
+    return this.get<ServerResponse<Activity[]>>('/user/activies', {
+      params: combineChainId(params, this.chainId)
+    });
   }
 
   rootHashUser(rootHash: string) {
-    return this.get<ServerResponse<{ address: string | null }>>(`/credential/${rootHash}/user`);
+    return this.get<ServerResponse<{ address: string | null }>>(`/credential/${rootHash}/user`, {
+      params: combineChainId({}, this.chainId)
+    });
   }
 }
