a7a885105b7ee96dfd4af229a2b57b7eb926a97f
Zhichao Zhang <zzcwoshizz@163.com>
2022-08-29 13:01:26 +0800

    fix import error

--- packages/did-keyring/src/utils.ts
+++ packages/did-keyring/src/utils.ts
@@ -1,6 +1,6 @@
 import type { DidKeys$Json } from './types';
 
-import { validateKiltDidUri } from '@kiltprotocol/did/lib/cjs/Did.utils';
+import { Utils } from '@kiltprotocol/did';
 
 export function isDidKeys$Json(json: unknown): json is DidKeys$Json {
   json as DidKeys$Json;
@@ -11,7 +11,7 @@ export function isDidKeys$Json(json: unknown): json is DidKeys$Json {
     (json as DidKeys$Json).keys.length === 2
   ) {
     try {
-      return validateKiltDidUri((json as DidKeys$Json).didUri);
+      return Utils.validateKiltDidUri((json as DidKeys$Json).didUri);
     } catch {
       return false;
     }
