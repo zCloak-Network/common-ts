e38c8deb0e6e50ea4f1e0505669f1d8483d49320
Zhichao Zhang <zzcwoshizz@163.com>
2022-09-05 12:44:05 +0800

    set password for did

--- packages/did-keyring/src/DidManager.ts
+++ packages/did-keyring/src/DidManager.ts
@@ -31,6 +31,12 @@ export class DidManager extends Events {
     this.emit('add');
   }
 
+  /**
+   * @description add a new did from mnemonic
+   * @param mnemonic 12 words mnemonic
+   * @param password (optional) if passed password, will call `backupDid` method
+   * @returns a [[LightDidDetails]] object
+   */
   public addDidFromMnemonic(mnemonic: string, password?: string): LightDidDetails {
     const pairs = [
       this.createFromUri(mnemonic, {}, 'sr25519'),
@@ -58,6 +64,12 @@ export class DidManager extends Events {
     return didDetails;
   }
 
+  /**
+   * @description add a new did from a [[DidKeys$Json]] or [[string]], if pass string, will covert to [[DidKeys$Json]]
+   * @param textOrJson [[DidKeys$Json]] or stringify(DidKeys$Json)
+   * @param password (optional) if passed password, will call `backupDid` method
+   * @returns a [[LightDidDetails]] object
+   */
   public addDidFromJson(textOrJson: string | DidKeys$Json, password?: string): LightDidDetails {
     let json: DidKeys$Json;
 
@@ -91,6 +103,12 @@ export class DidManager extends Events {
     return didDetails;
   }
 
+  /**
+   * @description backup did, use password to encrypt did json
+   * @param didUriOrDetails kilt didUri or LightDidDetails
+   * @param password did json file password
+   * @returns A [[DidKeysJson]] object.
+   */
   public backupDid(didUriOrDetails: DidUri | LightDidDetails, password: string): DidKeys$Json {
     let didDetails: LightDidDetails;
 
@@ -107,6 +125,39 @@ export class DidManager extends Events {
       didUri: didDetails.uri,
       keys: didDetails.getKeys().map((key) => this.getPair(key.publicKey).toJson(password))
     };
+  }
+
+  /**
+   * @description set a new password for did
+   * will call `backupDid` method
+   * @param didUriOrDetails kilt didUri or LightDidDetails
+   * @param currentPassword current password of passed did
+   * @param password new password for did
+   */
+  public setPassword(
+    didUriOrDetails: DidUri | LightDidDetails,
+    currentPassword: string,
+    password: string
+  ): void {
+    let didDetails: LightDidDetails;
+
+    if (didUriOrDetails instanceof LightDidDetails) {
+      didDetails = didUriOrDetails;
+    } else {
+      assert(Utils.validateKiltDidUri(didUriOrDetails), 'Not did uri');
+      assert(Utils.parseDidUri(didUriOrDetails).type === 'light', 'only light did uri backup');
+
+      didDetails = LightDidDetails.fromUri(didUriOrDetails);
+    }
+
+    didDetails.getKeys().forEach((key) => {
+      const pair = this.getPair(key.publicKey);
+
+      pair.unlock(currentPassword);
+      pair.encodePkcs8(password);
+    });
+
+    this.backupDid(didDetails, password);
   }
 
   public removeDid(didUriOrDetails: DidUri | LightDidDetails): void {
