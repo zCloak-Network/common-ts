7e3299726ff5ec13d1dc4e64a06dd3fc732ae201
alpers <alpers@163.com>
2023-01-04 21:32:37 +0800

    feat: add browser and extension session

---
+++ packages/ui-store/src/BrowserSession.ts
@@ -0,0 +1,27 @@
+// Copyright 2021-2022 zcloak authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import storage from 'store/storages/sessionStorage';
+import { BaseStore } from './BaseStore';
+
+export class BrowserStore extends BaseStore {
+  public all(fn: (key: string, value: unknown) => void): void {
+    storage.each((value: unknown, key: string): void => {
+      fn(key, value);
+    });
+  }
+
+  public get(key: string, fn: (value: unknown) => void): void {
+    fn(storage.read(key) as unknown);
+  }
+
+  public remove(key: string, fn?: () => void): void {
+    storage.remove(key);
+    fn && fn();
+  }
+
+  public set(key: string, value: unknown, fn?: () => void): void {
+    storage.write(key, value as string);
+    fn && fn();
+  }
+}
---
+++ packages/ui-store/src/ExtensionSession.ts
@@ -0,0 +1,32 @@
+// Copyright 2021-2022 zcloak authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { BaseStore } from './BaseStore';
+
+const session = chrome.storage.session;
+
+export class ExtensionSession extends BaseStore {
+  public all(fn: (key: string, value: string) => void) {
+    session.get(null, (items) => {
+      for (const key in items) {
+        fn(key, items[key]);
+      }
+    });
+  }
+
+  public get(key: string, fn: (value: unknown) => void) {
+    session.get(key, (item) => {
+      fn(item[key] as unknown);
+    });
+  }
+
+  public remove(key: string, fn?: () => void) {
+    session.remove(key);
+    fn && fn();
+  }
+
+  public set(key: string, value: unknown, fn?: () => void) {
+    session.set({ [key]: value });
+    fn && fn();
+  }
+}
